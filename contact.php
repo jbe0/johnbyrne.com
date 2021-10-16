2371b04f214dffb7f6f8339b4d495056f5fc88d9
jbe0 <jbe0@outlook.com>
2021-10-16 17:25:08 +0200

    Updates
    Added simple captcha and fixed linkedin link

--- contact.php
+++ contact.php
@@ -5,6 +5,8 @@ $to ="john@johnbyrne.com";
 $name = Trim(stripslashes($_POST['name'])); /*receives 'name' data through post method and stores it in 'name' variable*/
 $email = Trim(stripslashes($_POST['email']));
 $message = Trim(stripslashes($_POST['message']));
+$captcha = $_POST['captcha'];
+$captcha = (int)$captcha;
 
 $subject ="New message from website!";
 
@@ -19,9 +21,13 @@ $body .="Message: ";
 $body .=$message;
 $body .="\n";
 
-$go = mail($to, $subject, $body, "From:<$from>");
+if ($captcha == 15) {
+    $go = mail($to, $subject, $body, "From:<$from>");
+    header('Location: contact_thank_you.html');
+} else {
+    header('Location: contact_no_thank_you.html');
+}
 
-header('Location: contact_thank_you.html');
 exit();
 
  ?>

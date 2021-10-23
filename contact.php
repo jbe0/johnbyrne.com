5a57b21804dcec0a81d90c2ab950c077dceeac1d
jbe0 <jbe0@outlook.com>
2021-10-23 13:47:35 +0200

    Privacy Policy
    Included privacy policy to comply with GDPR.

--- contact.php
+++ contact.php
@@ -1,5 +1,8 @@
 <?php
 
+$file = 'email.log';
+$today = date("d/m/y");
+
 $from ="john@johnbyrne.com";
 $to ="john@johnbyrne.com";
 $name = Trim(stripslashes($_POST['name'])); /*receives 'name' data through post method and stores it in 'name' variable*/
@@ -7,6 +10,8 @@ $email = Trim(stripslashes($_POST['email']));
 $message = Trim(stripslashes($_POST['message']));
 $captcha = $_POST['captcha'];
 $captcha = (int)$captcha;
+$captcha2 = $_POST['captcha2'];
+$checkbox = $_POST['checkbox'];
 
 $subject ="New message from website!";
 
@@ -21,10 +26,12 @@ $body .="Message: ";
 $body .=$message;
 $body .="\n";
 
-if ($captcha == 15) {
+if (($captcha == 15) and (empty($captcha2)) and ($checkbox == True) ) {
     $go = mail($to, $subject, $body, "From:<$from>");
+    file_put_contents($file, $today.";Email sent.\n", FILE_APPEND);
     header('Location: contact_thank_you.html');
 } else {
+    file_put_contents($file, $today.";Email NOT sent.\n", FILE_APPEND);
     header('Location: contact_no_thank_you.html');
 }
 
